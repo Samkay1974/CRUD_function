82284cde637d5404fbda3e9855364ee116263957
Samkay1974 <samuel.ninson@ashesi.edu.gh>
2026-02-13 17:08:46 +0000

    db change

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 header("Content-Type: application/json");
 
 $host = "169.239.251.102";
-$dbname = "mobileapps_2026B_samuelninson";
+$dbname = "mobileapps_2026B_samuel_ninson";
 $username = "samuel.ninson";
 $password = "Sam@Ashesi2021";
 
